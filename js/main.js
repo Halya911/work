f1004290c5fc1abdb2fd8526031dbd0d660d7055
Halya911 <83021629+Halya911@users.noreply.github.com>
2021-04-22 18:34:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(function () {
+	$(window).scroll(function() {
+	    $(' .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .skill-time').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .skill-travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .skill-make').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.input').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+});
+$(function () {
+	$(window).scroll(function() {
+	    $('.input.information').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+});
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.btn-form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+});
+
